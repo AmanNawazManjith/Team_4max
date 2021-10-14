7b3e733baa619cc2a8ccbc4da4f5a9a2bb0c4ccd
Rahul Anand <57794377+CrAzYJOKR@users.noreply.github.com>
2021-10-14 16:39:51 +0530

    Delete team_4max directory

--- team_4max/models/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-start='1/30/2020'
-# start_date = datetime.datetime.strptime(start, '%m/%d/%Y')
-# future_forecast_dates = []
-# for i in range(len(future_forecast)):
-#     future_forecast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
