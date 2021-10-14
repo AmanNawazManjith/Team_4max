fe31c26c1f62b36fe1d732c3a21dce0056dc18dc
Aman Nawaz Manjith <71788604+AmanNawazManjith@users.noreply.github.com>
2021-10-14 16:41:23 +0530

    second commit

---
+++ apps/about.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def app():
+    st.title("About Page")
+
+    st.subheader("Developed By: Team 4Max")
+    st.success('Aman Nawaz Manjith')
+    st.caption('https://github.com/AmanNawazManjith')
+    st.success('Rahul Anand')
+    st.caption('https://github.com/CrAzYJOKR')
+    st.success('Linu Shibu')
+    st.caption('https://github.com/linu-shibu')
+    st.success('Aditya Manoj Bhaskaran')
+    st.caption('https://github.com/CrAzYJOKR')
+
+    
